837c512c5a95b221f9bc957ab3e1d6806ac562c1
ingean <inge.anundskas@gmail.com>
2024-04-25 11:35:44 +0200

    v1

--- create_storymap.py
+++ create_storymap.py
@@ -1,6 +1,6 @@
 import requests, io, os, json
 import urllib.request
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfFileReader
 
 from arcgis.gis import GIS
 from arcgis.apps.storymap import StoryMap
@@ -12,10 +12,11 @@ GPT_URL = "https://api.openai.com/v1/"
 GPT_TEXT_URL = f"{GPT_URL}chat/completions"
 GPT_IMAGE_URL = f"{GPT_URL}images/generations"
 
-ARCGIS_KEY = os.environ["ARCGIS_API_KEY"]
-gis = GIS(api_key=ARCGIS_KEY)
+AGOL_USER = os.environ["AGOL_USER"]
+AGOL_PWD = os.environ["AGOL_PWD"]
+gis = GIS(username=AGOL_USER, password=AGOL_PWD)
 
-config_file = open("config.json")
+config_file = open("config.json", encoding="utf-8")
 config = json.load(config_file)
 
 
@@ -57,16 +58,26 @@ def create_text(prompt):
   result = request_gpt(GPT_TEXT_URL, data)
   return result["message"]["content"]
   
-def text_from_pdf(url):  
+def text_from_pdf(url, page_numbers):  
   req = urllib.request.Request(url)
   remote_file = urllib.request.urlopen(req).read()
   remote_file_bytes = io.BytesIO(remote_file)
 
-  reader = PdfReader(remote_file_bytes)
-  page = reader.pages[0]
-  return page.extract_text()
+  reader = PdfFileReader(remote_file_bytes)
+  print(f"The PDF-document has {len(reader.pages)} pages") 
+
+  txt = ""
+  for page_nr in page_numbers:
+    page = reader.pages[page_nr]
+    txt += page.extractText() 
+  
+  if len(txt) > 15000:
+    txt = txt[0:15000]  # Cut text longer than approx. 4000 tokens (one token is approx 4 characters)
+
+  return txt
 
 def create_node(section):
+  print(f"Starting to create a {section['type']} section...")
   if section["type"] == "text":
     txt = create_text(section["prompt"])
     return Text(f"<strong>{section['title']}</strong><br>{txt}")
@@ -76,18 +87,19 @@ def create_node(section):
   if section["type"] == "map":
     return Map(section["item"])
   if section["type"] == "summary":
-    text = text_from_pdf(section["url"])
+    text = text_from_pdf(section["url"], section["pages"])
     prompt = f"Lag et sammendrag av følgende tekst: \n {text}"
     txt = create_text(prompt)
     return Text(f"<strong>{section['title']}</strong><br>{txt}")
   
 def create_storymap():
-  story = StoryMap()
+  story = StoryMap(gis=gis)
 
   for section in config["sections"]:
     story.add(create_node(section))
-    
-  story.cover(title=config["cover"]["title"], by_line="En historie skrevet av ChatGPT", image=create_image(config["cover"]["image"]))
+
+  cover_img = create_image(config["cover"]["image"])  
+  story.cover(title=config["cover"]["title"], by_line="En karthistorie fra ChatGPT", image=cover_img)
   story.save(title="Bodø by ChatGPT", publish=True)
   print(story.nodes)
 
